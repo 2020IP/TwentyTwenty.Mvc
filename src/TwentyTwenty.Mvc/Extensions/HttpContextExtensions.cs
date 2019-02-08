8376d37bd85758b07d06278ebe953756292a91ae
Eric Green <edgeric@gmail.com>
2019-02-08 13:12:16 -0600

    added httpcontext extension SetRemoteIpAddressToForwardedFor

---
+++ src/TwentyTwenty.Mvc/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using TwentyTwenty.BaseLine;
+
+namespace TwentyTwenty.Mvc
+{
+    public static class HttpContextExtensions
+    {
+        public static void SetRemoteIpAddressToForwardedFor(this HttpContext httpContext)
+        {
+            if (httpContext.Request.Headers.TryGetValue("X-Forwarded-For", out StringValues forwardValue))
+            {
+                SetRemoveIpAddress(forwardValue, httpContext);
+            }
+            else if (httpContext.Request.Headers.TryGetValue("Http-X-Forwarded-For", out StringValues httpForwardValue))
+            {
+                SetRemoveIpAddress(httpForwardValue, httpContext);
+            }
+        }
+
+        private static void SetRemoveIpAddress(StringValues value, HttpContext httpContext)
+        {
+            var forward = value[0].Split(new[] { ", ", "," }, StringSplitOptions.None)[0];
+
+            if (IPAddress.TryParse(forward, out IPAddress remoteIp))
+            {
+                httpContext.Connection.RemoteIpAddress = remoteIp;
+            }
+        }
+    }
+}
