e305c3b1e316d3b406a258964e2a146ed53d6cd2
Eric Green <edgeric@gmail.com>
2019-08-13 11:16:36 -0500

    custom dataannoationslocalization registration from IStringLocalizer

---
+++ src/TwentyTwenty.Mvc/Localization/JsonLocalizationMvcDataAnnotationsExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.Extensions.Localization;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class JsonLocalizationMvcDataAnnotationsExtensions
+    {
+        public static IMvcBuilder AddDataAnnotationsJsonLocalization(this IMvcBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            
+            builder.AddDataAnnotationsLocalization(o =>
+            {
+                var provider = builder.Services.BuildServiceProvider();
+                var localizer = provider.GetService<IStringLocalizer>();
+                o.DataAnnotationLocalizerProvider = (t, f) => localizer;
+            });
+
+            return builder;
+        }
+    }
+}
